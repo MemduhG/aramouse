75a21c0e9f08ce8e7f20ce5a5e2ad4e3596e3cc0
Memduh Gokirmak <memduh.gokirmak@cint.com>
2023-10-27 19:02:43 +0200

    more trials

--- content.js
+++ content.js
@@ -11,36 +11,59 @@ function handleMouseOver(event) {
      
       const target = event.target;
   
-      if (isArabicWord(target) && apiEndpoint) {
-        const word = target.textContent;
-        const postData = { word };
-  
-        const requestOptions = {
-          method: 'POST',
-          body: JSON.stringify(postData),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        };
-  
-        fetch(apiEndpoint, requestOptions)
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            } else {
-              throw new Error('API request failed');
-            }
-          })
-          .then((data) => {
-            browser.runtime.sendMessage({ apiResponse: data });
-          })
-          .catch((error) => {
-            console.error('API call failed:', error);
-          });
+      if (apiEndpoint) {
+        
+        const range = document.createRange();
+        range.selectNode(target);
+    
+        // Get the client rect of the range
+        const rect = range.getBoundingClientRect();
+
+        if (event.clientX >= rect.left && event.clientX <= rect.right
+            && event.clientY >= rect.top && event.clientY <= rect.bottom) {
+            const word = range.toString().trim();
+            console.log(word)
+
+            if (isArabicWord(word)) {
+                // Make the API call and proceed with your logic
+                makeAPICall(word);
+              }
+        } else {
+            console.log("not in the rect")
+        }
+
       }
     });
   }
 
+function makeAPICall(word) {
+    const postData = { word };
+    console.log(word)
+
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(apiEndpoint, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('API request failed');
+        }
+      })
+      .then((data) => {
+        browser.runtime.sendMessage({ apiResponse: data });
+      })
+      .catch((error) => {
+        console.error('API call failed:', error);
+      });
+}
+  
 
   function isArabicWord(element) {
     // Regular expression to match Arabic characters
